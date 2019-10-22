f48cd367a6bf6da9128f7670800fb7223c989fed
chu-hui-love <lovehui131125@gmail.com>
2019-10-22 08:51:37 +0800

    websocket

--- chat-web/src/main/java/com/chuhui/chat/web/config/WebSocketConfig.java
+++ chat-web/src/main/java/com/chuhui/chat/web/config/WebSocketConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurationSupport;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SubProtocolWebSocketHandler;
 
 import java.util.List;
 
@@ -18,13 +19,23 @@ import java.util.List;
  *
  * 可不可以选择继承{@link WebSocketMessageBrokerConfigurationSupport} 这个类呢?
  *
+ * {@link EnableWebSocketMessageBroker}只能和{@link WebSocketMessageBrokerConfigurer}这个接口配合使用
+ *
+ * 可以选择单独继承{@link WebSocketMessageBrokerConfigurationSupport}这个类
+ *
+ * 换句话说,添加 {@link EnableWebSocketMessageBroker}注解和继承{@link WebSocketMessageBrokerConfigurationSupport}二选一
+ *
+ *
+ *
+ *
+ *
  * @author: cyzi
  * @Date: 2019/10/18 0018
  * @Description:TODO
  */
 @Configuration
-//@EnableWebSocketMessageBroker
-public class WebSocketConfig extends WebSocketMessageBrokerConfigurationSupport {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 
 
@@ -36,11 +47,14 @@ public class WebSocketConfig extends WebSocketMessageBrokerConfigurationSupport 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chuhuichat/chws/").withSockJS()
+        registry.addEndpoint("/chws").withSockJS()
                 .setStreamBytesLimit(512 * 1024)
                 .setHttpMessageCacheSize(1000)
                 .setDisconnectDelay(30 * 1000);
     }
+
+//    SubProtocolWebSocketHandler
+
 
 //    @Override
 //    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
